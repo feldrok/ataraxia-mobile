0beda11f88e710a9c879275d32a42e130339ce94
feldrok <matias@moravalero.cl>
2023-02-13 09:22:55 -0300

    dev

--- navigation/DrawerNavigation.jsx
+++ navigation/DrawerNavigation.jsx
@@ -1,13 +1,36 @@
+import Cart from '../screens/Cart'
 import Home from '../screens/Home'
+import Ionicons from '@expo/vector-icons/Ionicons'
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 const Drawer = createDrawerNavigator()
+const Stack = createNativeStackNavigator()
 
-const DrawerNavigation = () => {
+const DrawerNavigation = ({ navigation }) => {
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator
+            screenOptions={{
+                headerRight: () => (
+                    <Ionicons
+                        name="cart-outline"
+                        size={30}
+                        color="#E0003F"
+                        style={{ marginRight: 10 }}
+                        onPress={() => navigation.navigate('Cart')}
+                    />
+                ),
+                drawerType: 'slide',
+                headerTintColor: '#E0003F',
+                drawerActiveBackgroundColor: '#E0003F',
+                drawerActiveTintColor: '#fff',
+                drawerInactiveBackgroundColor: '#fff',
+                drawerInactiveTintColor: '#E0003F',
+            }}
+        >
             <Drawer.Screen name="Home" component={Home} />
+            <Stack.Screen name="Cart" component={Cart} />
         </Drawer.Navigator>
     )
 }
